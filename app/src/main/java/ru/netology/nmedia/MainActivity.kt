ba8661122f5019932c8515916eb23a61fadcbe8b
Petukhov Konstantin <kostya49496@gmail.com>
2024-02-07 10:26:39 +0600

    Add arch MVVM

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -2,9 +2,10 @@ package ru.netology.nmedia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import ru.netology.nmedia.databinding.ActivityMainBinding
-import ru.netology.nmedia.dto.Post
 import ru.netology.nmedia.dto.PostService
+import ru.netology.nmedia.dto.PostViewModel
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,36 +13,27 @@ class MainActivity : AppCompatActivity() {
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val post = Post(
-            id = 1,
-            author = "Нетология. Университет интернет-профессий будущего",
-            content = "Привет, это новая Нетология! Когда-то Нетология начиналась с интенсивов по онлайн-маркетингу. Затем появились курсы по дизайну, разработке, аналитике и управлению. Мы растем сами и помогаем расти студентам: от новичков до уверенных профессионалов. Но самое важное остается с нами: мы верим, что в каждом уже есть сила, которая заставляет хотеть больше, целиться выше, бежать быстрее. Наша миссия - помочь встать на путь роста и начать цепочку перемен → http://netolo.gy/fyb",
-            publiched = "21 мая 18:36",
-            likeCount = 999,
-            likedByMe = false,
-            shareCount = 531_786
-        )
+        val viewModel: PostViewModel by viewModels()
 
-        with(binding){
-            author.text = post.author
-            published.text = post.publiched
-            content.text = post.content
-            if (post.likedByMe) {
-                like.setImageResource(R.drawable.ic_like_active)
-            }
-            likeCount.text = PostService.ConvertCountToShortString(post.likeCount)
-            shareCount.text = PostService.ConvertCountToShortString(post.shareCount)
-
-            like.setOnClickListener {
-                if (post.likedByMe) post.likeCount-- else post.likeCount++
+        viewModel.data.observe(this) {
+            post ->
+            with(binding){
+                author.text = post.author
+                published.text = post.publiched
+                content.text = post.content
+                like.setImageResource(if (post.likedByMe) R.drawable.ic_like_active else R.drawable.ic_like)
                 likeCount.text = PostService.ConvertCountToShortString(post.likeCount)
-                post.likedByMe = !post.likedByMe
-                like.setImageResource(if (post.likedByMe) R.drawable.ic_like_active else R.drawable.ic_like )
+                shareCount.text = PostService.ConvertCountToShortString(post.shareCount)
+            }
+        }
+
+        with(binding) {
+            like.setOnClickListener {
+                viewModel.like()
             }
 
             share.setOnClickListener{
-                post.shareCount++
-                shareCount.text = PostService.ConvertCountToShortString(post.shareCount)
+                viewModel.share()
             }
         }
     }
